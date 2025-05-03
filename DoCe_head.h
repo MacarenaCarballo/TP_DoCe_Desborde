7a6fccefb80eaa9fc8b4882242eb458eb69ff2e7
sheilarocha02 <rocha02.sheila@gmail.com>
2025-05-03 12:50:22 -0300

    agrega menú principal

--- DoCe_head.h
+++ DoCe_head.h
@@ -3,5 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #endif // DOCE_HEAD_H_INCLUDED
--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include "DoCe_head.h"
 #include "pila_head.h"
+
 int main()
 {
-    printf("Hello world!\n");
+    char opcion;
+    do
+    {
+        puts("--------MENU---------\n"
+             "[A] Jugar \n"
+             "[B] Ver ranking \n"
+             "[C] Salir \n"
+             "\nIngrese una opcion: \n");
+        scanf("%c", &opcion);
+        opcion = toupper(opcion);
+        fflush(stdin);
+
+        switch(opcion)
+        {
+
+        case 'A':
+
+            break;
+        case 'B':
+
+            break;
+        case 'C':
+            puts("\nSaliendo...\n");
+            break;
+        default:
+            puts("\nOpcion invalida. Intente de nuevo.\n");
+        }
+
+        system("pause");
+        system("cls");
+
+    }
+    while(opcion != 'C');
+
     return 0;
 }
