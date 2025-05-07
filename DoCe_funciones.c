24b7520133b23e1e6d22b5b722f8b1696d6e351f
Nelson <nelsonsalto2@gmail.com>
2025-05-06 22:33:48 -0300

    Add functions to generate deck and add cards

--- DoCe_funciones.c
+++ DoCe_funciones.c
@@ -1,3 +1,21 @@
 #include "DoCe_head.h"
-#include "pila_head.h"
 
+int agregarCartas(tPila* p, int tipo, int cantidad) {
+    for (int i = 0; i < cantidad; i++) {
+        if (apilar(p, &tipo, sizeof(int)) != SATISFACTORIO)
+            return PILA_LLENA;
+    }
+    return SATISFACTORIO;
+}
+
+int generarMazo(tPila *p) {
+
+    if (agregarCartas(p, MAS_DOS, 6) != SATISFACTORIO) return PILA_LLENA;
+    if (agregarCartas(p, MAS_UNO, 10) != SATISFACTORIO) return PILA_LLENA;
+    if (agregarCartas(p, SACAR_UNO, 8) != SATISFACTORIO) return PILA_LLENA;
+    if (agregarCartas(p, SACAR_DOS, 6) != SATISFACTORIO) return PILA_LLENA;
+    if (agregarCartas(p, REPETIR_TURNO, 6) != SATISFACTORIO) return PILA_LLENA;
+    if (agregarCartas(p, ESPEJO, 4) != SATISFACTORIO) return PILA_LLENA;
+
+    return SATISFACTORIO;
+}
--- DoCe_head.h
+++ DoCe_head.h
@@ -1,6 +1,8 @@
 #ifndef DOCE_HEAD_H_INCLUDED
 #define DOCE_HEAD_H_INCLUDED
 
+#include "pila_head.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -13,5 +15,9 @@
 #define ESPEJO 4
 #define GANO_MAQUINA 5
 #define GANO_HUMANO 6
+
+
+int generarMazo(tPila *p);
+int agregarCartas(tPila* p, int tipo, int cantidad);
 
 #endif // DOCE_HEAD_H_INCLUDED
