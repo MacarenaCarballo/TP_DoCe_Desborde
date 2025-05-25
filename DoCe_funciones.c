fda22f7100ae1f29bf72bc9ac30023552948d973
Nelson <nelsonsalto2@gmail.com>
2025-05-25 18:16:06 -0300

    Guardo cambios antes de mergear main

--- DoCe_funciones.c
+++ DoCe_funciones.c
@@ -1,21 +1,2 @@
 #include "DoCe_head.h"
 
-int apilarCartas(tPila *p, int *cartas, int cantidad) {
-    for (int i = 0; i < cantidad; i++) {
-        if (apilar(p, &cartas[i], sizeof(int)) != SATISFACTORIO)
-            return PILA_LLENA;
-    }
-    return SATISFACTORIO;
-}
-
-int generarMazo(tPila *p) {
-
-    if (agregarCartas(p, MAS_DOS, 6) != SATISFACTORIO) return PILA_LLENA;
-    if (agregarCartas(p, MAS_UNO, 10) != SATISFACTORIO) return PILA_LLENA;
-    if (agregarCartas(p, SACAR_UNO, 8) != SATISFACTORIO) return PILA_LLENA;
-    if (agregarCartas(p, SACAR_DOS, 6) != SATISFACTORIO) return PILA_LLENA;
-    if (agregarCartas(p, REPETIR_TURNO, 6) != SATISFACTORIO) return PILA_LLENA;
-    if (agregarCartas(p, ESPEJO, 4) != SATISFACTORIO) return PILA_LLENA;
-
-    return SATISFACTORIO;
-}
